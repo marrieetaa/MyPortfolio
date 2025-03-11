e3a4a1f41e3ef08b70fb3792d577b722d5e38548
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2025-03-11 14:27:55 +0400

    Add startTimer function in JS (step1.)

--- projects/clock/script.js
+++ projects/clock/script.js
@@ -3,3 +3,31 @@
 let hours = 0, minutes = 0, seconds = 0, milliseconds = 0;
 let timer;
 let running = false;
+
+//Start timer
+function startTimer() {
+    if(!running){
+        running = true;
+
+        timer = setInterval(() => {
+        milliseconds += 10;
+
+        if(milliseconds === 1000) {
+            milliseconds = 0;
+            seconds++;
+        }
+        if(seconds === 60) {
+            seconds = 0;
+            minutes++;
+        }
+        if(minutes === 60) {
+            minutes = 0;
+            hours++;
+        }
+        
+  },10)
+ }
+}
+
+const start = document.getElementById('start');
+start.addEventListener('click',startTimer);
