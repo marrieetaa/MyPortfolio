5f56b8758532920a9a8e2debca47f24e064fc3b8
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2025-03-11 14:56:31 +0400

    Create object to store initial variables and use as object properties

--- projects/clock/script.js
+++ projects/clock/script.js
@@ -1,28 +1,33 @@
 //--Stopwatch--
-//Initial variables
-let hours = 0, minutes = 0, seconds = 0, milliseconds = 0;
-let timer;
-let running = false;
+//Stopwatch object
+const stopwatch = {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+    milliseconds: 0,
+    timer: null,
+    running: false
+}
 
-//Start timer
+//Start Stopwatch
 function startTimer() {
-    if(!running){
-        running = true;
+    if(!stopwatch.running){
+        stopwatch.running = true;
 
-        timer = setInterval(() => {
-        milliseconds += 10;
+        stopwatch.timer = setInterval(() => {
+            stopwatch.milliseconds += 10;
 
-        if(milliseconds === 1000) {
-            milliseconds = 0;
-            seconds++;
+        if(stopwatch.milliseconds === 1000) {
+            stopwatch.milliseconds = 0;
+            stopwatch.seconds++;
         }
-        if(seconds === 60) {
-            seconds = 0;
-            minutes++;
+        if(stopwatch.seconds === 60) {
+            stopwatch.seconds = 0;
+            stopwatch.minutes++;
         }
-        if(minutes === 60) {
-            minutes = 0;
-            hours++;
+        if(stopwatch.minutes === 60) {
+            stopwatch.minutes = 0;
+            stopwatch.hours++;
         }
         updateDisplay();
         
@@ -30,28 +35,29 @@ function startTimer() {
  }
 }
 
-//Stop timer
+//Stop Stopwatch
 function stopTimer() {
-    running = false;
-    clearInterval(timer);
+    stopwatch.running = false;
+    clearInterval(stopwatch.timer);
 }
 
-//Reset timer
+//Reset Stopwatch
 function resetTimer() {
-   running = false;
-   clearInterval(timer);
-   hours = 0;
-   minutes = 0;
-   seconds = 0; 
-   milliseconds = 0;
+    stopwatch.running = false;
+   clearInterval(stopwatch.timer);
+   stopwatch.hours = 0;
+   stopwatch.minutes = 0;
+   stopwatch.seconds = 0; 
+   stopwatch.milliseconds = 0;
    updateDisplay();
 }
 
+//Update the time on screen
 function updateDisplay(){
-   document.getElementById('hours').innerText = String(hours).padStart(2,'0'); //00 format
-   document.getElementById('minutes').innerText = String(minutes).padStart(2,'0');
-   document.getElementById('seconds').innerText = String(seconds).padStart(2,'0');
-   document.getElementById('milliseconds').innerText = String(Math.floor(milliseconds / 10)).padStart(2,'0');
+   document.getElementById('hours').innerText = String(stopwatch.hours).padStart(2,'0'); //00 format
+   document.getElementById('minutes').innerText = String(stopwatch.minutes).padStart(2,'0');
+   document.getElementById('seconds').innerText = String(stopwatch.seconds).padStart(2,'0');
+   document.getElementById('milliseconds').innerText = String(Math.floor(stopwatch.milliseconds / 10)).padStart(2,'0');
 }
 
 const start = document.getElementById('start-btn');
