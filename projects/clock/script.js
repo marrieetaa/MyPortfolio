367c99c540329a8a45d5901964aa8c4b1073b576
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2025-03-11 16:20:35 +0400

    Refator code for better readability by grouping variables and functions into 'stopwath' object

--- projects/clock/script.js
+++ projects/clock/script.js
@@ -6,58 +6,51 @@ const stopwatch = {
     seconds: 0,
     milliseconds: 0,
     timer: null,
-    running: false
-}
-
-//Start Stopwatch
-function startTimer() {
-    if(!stopwatch.running){
-        stopwatch.running = true;
-
-        stopwatch.timer = setInterval(() => {
-            stopwatch.milliseconds += 10;
-
-        if(stopwatch.milliseconds === 1000) {
-            stopwatch.milliseconds = 0;
-            stopwatch.seconds++;
+    running: false,
+    startTimer() {
+        if(!this.running) {
+            this.running = true;
+            this.timer = setInterval(() => this.updateTime(),10);
         }
-        if(stopwatch.seconds === 60) {
-            stopwatch.seconds = 0;
-            stopwatch.minutes++;
+    },
+    stopTimer() {
+        this.running = false;
+        clearInterval(this.timer);
+    },
+    resetTimer() {
+        this.running = false;
+        clearInterval(this.timer);
+        this.hours = 0;
+        this.minutes = 0;
+        this.seconds = 0;
+        this.milliseconds = 0;
+        this.updateDisplay();
+    },
+    updateTime() {
+        this.milliseconds += 10;
+        if(this.milliseconds === 1000) {
+            this.milliseconds = 0;
+            this.seconds++;
         }
-        if(stopwatch.minutes === 60) {
-            stopwatch.minutes = 0;
-            stopwatch.hours++;
+        if(this.seconds === 60) {
+            this.seconds = 0;
+            this.minutes++;
         }
-        updateDisplay();
-        
-  },10)
- }
-}
-
-//Stop Stopwatch
-function stopTimer() {
-    stopwatch.running = false;
-    clearInterval(stopwatch.timer);
-}
-
-//Reset Stopwatch
-function resetTimer() {
-    stopwatch.running = false;
-   clearInterval(stopwatch.timer);
-   stopwatch.hours = 0;
-   stopwatch.minutes = 0;
-   stopwatch.seconds = 0; 
-   stopwatch.milliseconds = 0;
-   updateDisplay();
-}
-
-//Update the time on screen
-function updateDisplay(){
-   document.getElementById('hours').innerText = String(stopwatch.hours).padStart(2,'0'); //00 format
-   document.getElementById('minutes').innerText = String(stopwatch.minutes).padStart(2,'0');
-   document.getElementById('seconds').innerText = String(stopwatch.seconds).padStart(2,'0');
-   document.getElementById('milliseconds').innerText = String(Math.floor(stopwatch.milliseconds / 10)).padStart(2,'0');
+        if(this.minutes === 60) {
+            this.minutes = 0;
+            this.hours++;
+        }
+        this.updateDisplay();
+    },
+    updateDisplay() {
+      document.getElementById('hours').innerText = this.formatTime(this.hours);
+      document.getElementById('minutes').innerText = this.formatTime(this.minutes);
+      document.getElementById('seconds').innerText = this.formatTime(this.seconds);
+      document.getElementById('milliseconds').innerText = this.formatTime(Math.floor(this.milliseconds / 10));
+    },
+    formatTime(time) {
+        return String(time).padStart(2,'0'); 
+    }
 }
 
 //Selecting the buttons
@@ -66,6 +59,6 @@ const stop = document.getElementById('stop-btn');
 const reset  = document.getElementById('reset-btn');
 
 //Binding functions to the buttons
-start.addEventListener('click',startTimer);
-stop.addEventListener('click',stopTimer);
-reset.addEventListener('click',resetTimer);
+start.addEventListener('click',() => stopwatch.startTimer());
+stop.addEventListener('click',() => stopwatch.stopTimer());
+reset.addEventListener('click',() => stopwatch.resetTimer());
